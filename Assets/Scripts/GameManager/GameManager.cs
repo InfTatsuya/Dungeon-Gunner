b93d082e7597450d43df4a33f2ef2840ec1ae180
InfTatsuya <107315017+InfTatsuya@users.noreply.github.com>
2022-11-07 16:48:31 +0700

    Room class's skeleton + Shader

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[DisallowMultipleComponent]
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    [Space(10)]
+    [Header("DUNGEON LEVELS"), Tooltip("Populate with the dungeon level SO")]
+    [SerializeField] private List<DungeonLevelSO> dungeonLevelList;
+
+    [SerializeField] private int currentDungeonLevelListIndex = 0;
+
+    [HideInInspector] public GameState gameState;
+
+    void Start()
+    {
+        gameState = GameState.gameStarted;
+    }
+
+    void Update()
+    {
+        HandleGameState();
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            gameState = GameState.gameStarted;
+        }
+    }
+
+    private void HandleGameState()
+    {
+        switch (gameState)
+        {
+            case GameState.gameStarted:
+                PlayDungeonLevel(currentDungeonLevelListIndex);
+                gameState = GameState.playingLevel;
+                break;
+
+
+        }
+    }
+
+    private void PlayDungeonLevel(int dungeonLevelListIndex)
+    {
+
+    }
+
+    #region Validation  
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(dungeonLevelList), dungeonLevelList);
+
+    }
+
+#endif
+
+    #endregion
+}
