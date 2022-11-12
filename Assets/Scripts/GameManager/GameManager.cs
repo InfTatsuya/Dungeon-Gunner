fb4c92e4193d1ef30979805a7463a71bcdb2b35b
InfTatsuya <107315017+InfTatsuya@users.noreply.github.com>
2022-11-13 00:30:12 +0700

    Implement Object Pooling

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -36,6 +36,21 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
         player.Initialize(playerDetails);
     }
 
+    private void OnEnable()
+    {
+        StaticEventHandler.OnRoomChanged += StaticEventHandler_OnRoomChanged;
+    }
+
+    private void OnDisable()
+    {
+        StaticEventHandler.OnRoomChanged -= StaticEventHandler_OnRoomChanged;
+    }
+
+    private void StaticEventHandler_OnRoomChanged(RoomChangedEventArgs roomChangedEventArgs)
+    {
+        SetCurrentRoom(roomChangedEventArgs.room);
+    }
+
     void Start()
     {
         gameState = GameState.gameStarted;
